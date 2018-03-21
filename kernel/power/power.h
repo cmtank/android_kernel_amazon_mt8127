d81372baa65e10d8270843db1de16a12aec1a86a
Stricted <info@stricted.net>
2018-03-21 23:40:26 +0100

    fix compilation after merge

--- kernel/power/power.h
+++ kernel/power/power.h
@@ -190,6 +190,7 @@ struct pm_sleep_state {
 /* kernel/power/suspend.c */
 extern struct pm_sleep_state pm_states[];
 
+extern bool valid_state(suspend_state_t state);
 extern int suspend_devices_and_enter(suspend_state_t state);
 //<20130327> <marc.huang> merge from android kernel 3.0 - add enter_state declarition when CONFIG_SUSPEND is defined
 extern int enter_state(suspend_state_t state);
