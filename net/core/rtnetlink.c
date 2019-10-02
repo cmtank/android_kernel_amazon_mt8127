292edd523e106a794df817e470b4713207bf3583
diego-cr <diegocr@gmail.com>
2019-10-02 11:37:29 +0200

    Merge tag 'v3.10.102' into HEAD
    This is the 3.10.102 stable release

--- net/core/rtnetlink.c
+++ net/core/rtnetlink.c
@@ -910,15 +910,15 @@ static int rtnl_fill_ifinfo(struct sk_buff *skb, struct net_device *dev,
 		goto nla_put_failure;
 
 	if (1) {
-        struct rtnl_link_ifmap map;
+		struct rtnl_link_ifmap map;
 
-	    memset(&map, 0, sizeof(map));
-	    map.mem_start   = dev->mem_start;
-	    map.mem_end     = dev->mem_end;
-	    map.base_addr   = dev->base_addr;
-	    map.irq         = dev->irq;
-	    map.dma         = dev->dma;
-	    map.port        = dev->if_port;		
+		memset(&map, 0, sizeof(map));
+		map.mem_start   = dev->mem_start;
+		map.mem_end     = dev->mem_end;
+		map.base_addr   = dev->base_addr;
+		map.irq         = dev->irq;
+		map.dma         = dev->dma;
+		map.port        = dev->if_port;
 
 		if (nla_put(skb, IFLA_MAP, sizeof(map), &map))
 			goto nla_put_failure;
